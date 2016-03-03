993c9a1d18b9833d8f8e558d83a62b6e7df31e4f
KarenCastelan <karen_co13@outlook.com>
2016-03-03 13:13:37 -0600

    Karen
    añadi mi bean

---
+++ Parcial2/src/java/Beans/EmpleadoBean.java
@@ -0,0 +1,115 @@
+package Beans;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="formEM")
+@RequestScoped
+public class EmpleadoBean {
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public Integer getCp() {
+        return cp;
+    }
+
+    public void setCp(Integer cp) {
+        this.cp = cp;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNumseguro() {
+        return numseguro;
+    }
+
+    public void setNumseguro(String numseguro) {
+        this.numseguro = numseguro;
+    }
+private String clave;    
+private String nombre;
+private String calle;
+private Integer numero;
+private String colonia;
+private Integer cp;
+private String ciudad;
+private String estado;
+private String  telefono;
+private String numseguro;
+
+    public EmpleadoBean() {
+    }
+    public String registrar(){
+        return "confirmationEmpleados";
+    }
+    
+    public String index(){
+        return "index";
+    }
+}
