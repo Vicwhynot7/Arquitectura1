e0594bf53e3114555756be6c8887859db47cd3fb
Vicwhynot7 <vicwhynot7@gmail.com>
2016-03-03 16:51:29 -0600

    Victor
    Agregue mi bean

---
+++ Parcial2/src/java/Beans/SucursalesBean.java
@@ -0,0 +1,113 @@
+package Beans;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "formS")
+@RequestScoped
+public class SucursalesBean {
+
+    private String id;
+    private String nombre;
+    private Integer cp;
+    private String ciudad;
+    private String estado;
+    private String telefono;
+    private String fax;
+    private String contacto;
+    private String rfc;
+
+
+
+    public Integer getCp() {
+        return cp;
+    }
+
+    public void setCp(Integer cp) {
+        this.cp = cp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+ 
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public SucursalesBean() {
+    }
+
+    public String registrar() {
+        return "confirmationSucursales";
+    }
+
+    public String index() {
+        return "index";
+    }
+
+}
+
